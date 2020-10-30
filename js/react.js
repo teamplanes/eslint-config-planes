8f8559fe64a18b4528052d53704e40365e58837d
Henry <henry@kirkness.com>
2020-10-30 11:19:31 +0000

    feat: add js only config

---
+++ js/react.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "plugins": ["prettier", "react-hooks"],
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "plugin:react-hooks/recommended"
+  ],
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx"]
+      }
+    }
+  },
+  "rules": {
+    "react/jsx-props-no-spreading": 0,
+    "react/jsx-filename-extension": ["error", { "extensions": [".ts", ".tsx", ".js", ".jsx"] }],
+    "import/prefer-default-export": 0,
+    "import/no-default-export": 2,
+    "prettier/prettier": "error",
+    "react/destructuring-assignment": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    'import/extensions': 0
+  }
+}
